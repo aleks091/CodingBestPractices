ae8c3f8a533c50070bb26a33b084db626613a685
aleks091 <alejandro.trujillo.20@gmail.com>
2015-08-06 22:34:06 -0700

    Added redundant switch

--- DrySwitch/Payroll.cs
+++ DrySwitch/Payroll.cs
@@ -4,6 +4,19 @@ namespace DrySwitch
 {
     public class Payroll
     {
+        public bool IsPayday(Employee e, DateTime date)
+        {
+            switch (e.Type)
+            {
+                case EmployeeType.Salary:
+                    return date.Day == 5 || date.Day == 20;
+                case EmployeeType.Hourly:
+                    return date.Day == 10 || date.Day == 25;
+                default:
+                    throw new InvalidOperationException(e.Type.ToString());
+            }
+        }
+
         public decimal CalculatePay(Employee e)
         {
             switch (e.Type)
