e751ebce65847664b018a54e58c0ab95e6dfe993
aleks091 <alejandro.trujillo.20@gmail.com>
2015-08-14 00:53:46 -0700

    Incluido Proyecto de Refactorizacion del Codigo

---
+++ RefactorizacionTests/ReenvioLlamadasTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefactorizacionTests
+{
+    [TestClass]
+    public class ReenvioLlamadasTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
